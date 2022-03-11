a3c8adda5048e56d44b5109eb137e12f78e2c50b
douglasitsoares <dogametal@hotmail.com>
2022-03-11 20:20:26 -0300

    006_RoutingChanged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
-import { PostListComponent } from "./posts/post-list/post-list.component";
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { ScheduleListComponent } from "./schedules/schedule-list/schedule-list.component";
 import { ScheduleCreateComponent } from "./schedules/schedule-create/schedule-create.component";
 import { LoginComponent } from "./auth/login/Login.component";
@@ -13,8 +11,6 @@ import { AuthGuard } from "./auth/auth.guard";
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'schedule', component: ScheduleListComponent, canActivate: [AuthGuard]},
-  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard]},
-  { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard]},
   { path: 'schedule/create', component: ScheduleCreateComponent, canActivate: [AuthGuard]},
   { path: 'schedule/edit/:scheduleId', component: ScheduleCreateComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,10 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { ScheduleCreateComponent } from "./schedules/schedule-create/schedule-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { LoginComponent } from "./auth/login/Login.component";
 import { SignupComponent } from "./auth/signup/Signup.component";
-import { PostListComponent } from "./posts/post-list/post-list.component";
 import { ScheduleListComponent } from "./schedules/schedule-list/schedule-list.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthInterceptor } from "./auth/auth-interceptor";
@@ -25,10 +23,8 @@ import { AngularMaterialModule } from "./angular-material.module";
   declarations: [
     AppComponent,
     HomeComponent,
-    PostCreateComponent,
     ScheduleCreateComponent,
     HeaderComponent,
-    PostListComponent,
     ScheduleListComponent,
     LoginComponent,
     SignupComponent,
